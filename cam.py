989fcd53bacc6c8e51c0e93dbdd2e50012527cd8
micheleberetta98 <beretta.m98@gmail.com>
2022-07-10 12:09:27 +0200

    Rework camera for speed

--- cam.py
+++ cam.py
@@ -1,6 +1,5 @@
 from collections import deque
 import argparse
-import imutils
 import cv2
 
 
@@ -10,19 +9,28 @@ class Color:
         self.upper = upper
 
 
+crop_offset = (20, 110)
+crop_size = 420
+scale = 0.5
+center = (crop_size * scale / 2, crop_size * scale / 2)
+
+
 class Camera:
     def __init__(self, cid=0, show_windows=False):
         self.camera = cv2.VideoCapture(cid)
+        self.set_resolution(640, 480)
+
         self.black = Color((0, 0, 0), (179, 255, 50))
         self.orange = Color((5, 50, 50), (15, 255, 255))
         self._show_windows = show_windows
 
     def read_position(self, colors='all'):
         (grabbed, frame) = self.camera.read()
-        frame = imutils.resize(frame, width=600)
+        frame = self._rescale(self._crop(frame))
 
-        c_orange, mask_orange = self._get_center(self.orange, frame)
-        c_black, mask_black = self._get_center(self.black, frame)
+        c_black = (0, 0)
+        c_orange = self._get_center(self.orange, frame)
+        # c_black = self._get_center(self.black, frame)
 
         if self._show_windows:
             cv2.imshow("Frame", frame)
@@ -36,9 +44,25 @@ class Camera:
         if colors == 'black':
             return c_black
 
-    def release():
+    def release(self):
         self.camera.release()
         cv2.destroyAllWindows()
+
+    def set_resolution(self, width, height):
+        self.camera.set(3, width)
+        self.camera.set(4, height)
+
+    def _rescale(self, frame):
+        frame = cv2.resize(frame,
+                           (int(frame.shape[0] * scale), int(frame.shape[1] * scale)))
+        return frame
+
+    def _crop(self, frame):
+        xstart = crop_offset[0]
+        xend = xstart + crop_size
+        ystart = crop_offset[1]
+        yend = ystart + crop_size
+        return frame[xstart:xend, ystart:yend]
 
     def _get_center(self, color, frame):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -46,16 +70,15 @@ class Camera:
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
 
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnts = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
 
-            if radius > 10:
-                if self._show_windows:
-                    cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                return (x, y), mask
+            if self._show_windows:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 
-        return None, mask
+            return (x, y)
+
+        return None

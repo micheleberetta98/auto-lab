a587809ef77a0774aed11b978ca63ea672a3d87d
micheleberetta98 <beretta.m98@gmail.com>
2022-07-23 10:26:51 +0200

    Basic running/stopping the server

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request
-# from servo import Servo
+from multiprocessing import Process
+from time import sleep
 
 app = Flask(__name__)
 
-# s1 = Servo(12)
-# s2 = Servo(13)
+
+class PIDProcess():
+    def __init__(self):
+        self.p = None
+
+    def start(self, target):
+        self.stop()
+        self.p = Process(target=target)
+        self.p.start()
+
+    def stop(self):
+        if self.p is not None:
+            self.p.terminate()
+
+
+p = PIDProcess()
 
 
 @app.route('/')
@@ -14,4 +29,19 @@ def index():
 
 @app.route('/pid-center')
 def pid_center():
+    global p
+    p.start(handle_center)
     return ''
+
+
+@app.route('/pid-stop')
+def pid_stop():
+    global p
+    p.stop()
+    return ''
+
+
+def handle_center():
+    while True:
+        print('running...')
+        sleep(1)

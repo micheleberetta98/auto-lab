ccd7a1f68b941fdada6cb967b9b523d7f1e387a8
micheleberetta98 <beretta.m98@gmail.com>
2022-07-23 11:51:35 +0200

    Add main_loop target parametrization

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from servo import Servo
 from simple_pid import PID
 
 
-def main_loop(open_camera_frame=False):
+def main_loop(target, open_camera_frame=False):
     s1 = Servo(12, base=BASE_X)
     s2 = Servo(13, base=BASE_Y)
 
     camera = Camera(show_windows=open_camera_frame)
-    pid_x = create_pid(center[0])
-    pid_y = create_pid(center[1], Ku=0.130)
+    pid_x = create_pid(target[0])
+    pid_y = create_pid(target[1], Ku=0.130)
 
     while True:
         orange = camera.read_position()
@@ -30,7 +30,7 @@ def main_loop(open_camera_frame=False):
 
             print(f'Time: {datetime.now()}')
             print(f'\tAngle x = {control_x}\tAngle y = {control_y}')
-            print(f'\tDesired = {center}\tActual = ({x}, {y})')
+            print(f'\tDesired = {target}\tActual = ({x}, {y})')
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -39,4 +39,4 @@ def main_loop(open_camera_frame=False):
 
 
 if __name__ == '__main__':
-    main_loop(open_camera_frame=False)
+    main_loop(target=center, open_camera_frame=False)

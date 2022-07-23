db9ab5070f5b854fa9246eaa4a656db10efca5b3
micheleberetta98 <beretta.m98@gmail.com>
2022-07-23 09:29:26 +0200

    Create control.py

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
 import cv2
-from collections import deque
 from cam import Camera, center
+from collections import deque
+from control import create_pid, BASE_X, BASE_Y
 from datetime import datetime
-from simple_pid import PID
 from servo import Servo
+from simple_pid import PID
 
-Ku = 0.124
-Tu = 10.14  # secondi
-
-Kp = 0.6 * Ku
-Ki = 1.2 * Ku / Tu
-Kd = 3 * Ku * Tu / 40
-
-output_limits = (-60, 60)
-sample_time = 0.01
 
 if __name__ == '__main__':
     s1 = Servo(12)
     s2 = Servo(13)
 
     camera = Camera(show_windows=True)
-    pid_x = PID(Kp, Ki, Kd, setpoint=center[0], output_limits=output_limits, sample_time=sample_time)
-    pid_y = PID(Kp, Ki, Kd, setpoint=center[1], output_limits=output_limits, sample_time=sample_time)
+    pid_x = create_pid(center[0], BASE_X)
+    pid_y = create_pid(center[1], BASE_Y)
 
     while True:
         orange = camera.read_position()
         if orange is None:
-            s1.move_to(-10)
-            s2.move_to(-15)
+            s1.move_to(BASE_X)
+            s2.move_to(BASE_Y)
         else:
             (x, y) = orange
             control_x = pid_x(x)
             control_y = pid_y(y)
 
-            s1.move_to(control_x - 10)
-            s2.move_to(control_y - 15)
+            s1.move_to(control_x)
+            s2.move_to(control_y)
 
             print(f'Time: {datetime.now()}')
             print(f'\tAngle x = {control_x}\t\tAngle y = {control_y}')
